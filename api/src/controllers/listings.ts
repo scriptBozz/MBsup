9ac9f52fb6c21bb898168c5e987e801d6474a454
uche <scryptos@protonmail.com>
2023-08-19 16:09:19 +0200

    removed nested objects in models

--- api/src/controllers/listings.ts
+++ api/src/controllers/listings.ts
@@ -7,6 +7,25 @@ export const createListings = async (req: Request, res: Response) => {
     title: req.body.title,
     status: req.body.status,
     cost: req.body.cost,
+
+    number_of_rooms: req.body.number_of_rooms,
+    number_of_bedrooms: req.body.number_of_bedrooms,
+
+    type: req.body.type,
+    delivery_level: req.body.delivery_level,
+
+    address: req.body.address,
+    zipcode: req.body.zipcode,
+    street: req.body.street,
+    town: req.body.town,
+    city: req.body.city,
+    district: req.body.district,
+    province: req.body.province,
+
+    available_since: req.body.available_since,
+    broker_name: req.body.broker_name,
+    broker_email: req.body.broker_email,
+    broker_phone: req.body.broker_phone,
   });
   try {
     const Listings = await createListingService(listingInfo);
